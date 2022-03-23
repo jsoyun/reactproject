2875918ccb16be8cf6d94768b634ef68fea09fff
jsoyun <syusmm233@gmail.com>
2022-03-23 17:35:57 +0900

    movie 진행중

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,21 @@
-import Todo from "./components/Todo";
-import CoinTracker from "./components/CoinTracker";
+import { useState, useEffect } from "react";
 function App() {
-  return (
-    <div>
-      <CoinTracker />
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year73`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+
+    //로딩을 끝내서 setLoading을 false화면 보여주는 걸로 바꿔야함
+  }, []);
+  console.log(movies, "dfdfdfdf");
+  return <div>{loading ? <h1>loading....</h1> : null}</div>;
 }
 
 export default App;
