2d4ef48bd3164ac7e7256c3520545837c3609bbd
jsoyun <syusmm233@gmail.com>
2022-03-24 15:45:53 +0900

    state수업중

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,24 @@
-import { useState, useEffect } from "react";
-function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year73`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+import React from "react";
 
-    //로딩을 끝내서 setLoading을 false화면 보여주는 걸로 바꿔야함
-  }, []);
-  console.log(movies, "dfdfdfdf");
-  return <div>{loading ? <h1>loading....</h1> : null}</div>;
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((curr) => ({ count: curr.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
