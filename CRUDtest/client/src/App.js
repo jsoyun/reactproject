c0752e23d3d6b5484b160e42092ca7e7a7447f27
jsoyun <syusmm233@gmail.com>
2022-04-17 17:50:16 +0900

    jsconfig 삭제

--- CRUDtest/client/src/App.js
+++ CRUDtest/client/src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import LoginPage from "pages/LoginPage";
-import Home from "pages/Home";
+import LoginPage from "./pages/LoginPage";
+import Home from "./pages/Home";
 function App() {
   return (
     <Routes>
