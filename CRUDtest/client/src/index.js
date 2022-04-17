ae868d0dc6f798aeedbabe9a9af0abfe694cbc41
jsoyun <syusmm233@gmail.com>
2022-04-17 18:14:34 +0900

    헤더 푸터 수정

--- CRUDtest/client/src/index.js
+++ CRUDtest/client/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
@@ -11,11 +9,9 @@ ReactDOM.render(
   //BrowserRouter 웹 애플리케이션에 HTML5의 History API를 사용하여
   //페이지를 새로 불러오지 않고도 주소를 변경하고 현재 주소의 경로에 관련된 정보를 리액트 컴포넌트에서 사용할 수 있도록 해 줌
   <BrowserRouter>
-    <Header />
     {/* <React.StrictMode> */}
     <App />
     {/* </React.StrictMode> */}
-    <Footer />
   </BrowserRouter>,
   document.getElementById("root")
 );
--- CRUDtest/client/src/pages/Home.js
+++ CRUDtest/client/src/pages/Home.js
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 const Home = () => {
   return (
     <div>
+      <Header />
       <h1>홈</h1>
+
       <Link to="/login">로그인</Link>
+      <Footer />
     </div>
   );
 };
