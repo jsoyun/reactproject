c4c1a4b8f360437bb6460986b073dfabb4b1831d
jsoyun <syusmm233@gmail.com>
2022-04-25 16:13:28 +0900

    scrollView

--- reactNative/SoyunWeather/App.js
+++ reactNative/SoyunWeather/App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light"></StatusBar>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울은</Text>
+      </View>
+      <ScrollView horizontal contentContainerStyle={styles.whether}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>맑음</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>맑음</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>맑음</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>맑음</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +34,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "teal",
+  },
+  city: {
+    flex: 1,
+    margin: "100",
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  cityName: {
+    fontSize: 68,
+    fontStyle: "italic",
+    color: "white",
+    fontWeight: "500",
+  },
+  whether: {
+    backgroundColor: "orange",
+  },
+  day: {
+    alignItems: "center",
+  },
+  temp: {
+    color: "white",
+    marginTop: 18,
+    fontSize: 180,
+  },
+  description: {
+    color: "teal",
+    marginTop: -20,
+    fontSize: 60,
   },
 });
